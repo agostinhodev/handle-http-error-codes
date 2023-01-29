c71fd28df67d67f6e53f0bd3fbd4e29360662f77
agostinhodev <agostinho.developer@gmail.com>
2023-01-29 20:28:32 -0300

    feat: :sparkles: Added some basic test examples

--- src/__tests__/404.test.ts
+++ src/__tests__/404.test.ts
@@ -1,5 +1,5 @@
 import handleHTTPErrorCodes from '../index';
 
-test('Expected status codes', () => {
+test('Expected 404', () => {
     expect(handleHTTPErrorCodes(404)).toBe('We were unable to find the resource you are looking for. Try again');
 });
