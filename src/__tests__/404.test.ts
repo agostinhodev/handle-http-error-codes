9b120a0e4184133262d4212a91856f5426794c48
agostinhodev <agostinho.developer@gmail.com>
2023-01-29 20:52:50 -0300

    feat: :white_check_mark: Updated test

--- src/__tests__/404.test.ts
+++ src/__tests__/404.test.ts
@@ -1,4 +1,4 @@
-import handleHTTPErrorCodes from '../index';
+import { handleHTTPErrorCodes } from '../index';
 
 test('Expected 404', () => {
     expect(handleHTTPErrorCodes(404)).toBe('We were unable to find the resource you are looking for. Try again');
--- src/__tests__/UnknowStatusCode.test.ts
+++ src/__tests__/UnknowStatusCode.test.ts
@@ -1,4 +1,4 @@
-import handleHTTPErrorCodes from '../index';
+import { handleHTTPErrorCodes } from '../index';
 
 test('Expected Unknow status code', () => {
     expect(handleHTTPErrorCodes(999)).toBe('An unknown error has occurred');
